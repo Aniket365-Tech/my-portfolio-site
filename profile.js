1a8d8c2c31ecc0ca9531489f403f35b083ab5f6f
Aniket <aniketgajbhiye46@gmail.com>
2025-05-17 17:39:46 +0530

    add about and profile section

--- profile.js
+++ profile.js
@@ -1,12 +1,15 @@
 
-var tablinks = document.getelementByClassName(tab-titles)
-var tabcontents = document.getelementByClassName(tab-contents)
+var tablinks = document.getElementsByClassName("tab-links")
+var tabcontents = document.getElementsByClassName("tab-contents")
 
 function opentab(tabname){
-    for(tablinks of tablinks){
-        tablinks.classList.remove("active-link")
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link");
     }
-    for(tabcontents of tabcontents){
-        tabcontents.classList.remove("active-tab")
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
     }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+    
 }
