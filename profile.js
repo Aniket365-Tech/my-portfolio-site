0f3408892a21d1c8913f4e05925e4fffe402699a
Aniket <aniketgajbhiye46@gmail.com>
2025-05-18 17:50:42 +0530

    complet the project

--- profile.js
+++ profile.js
@@ -13,3 +13,29 @@ function opentab(tabname){
     document.getElementById(tabname).classList.add("active-tab");
     
 }
+
+var sidemenu = document.getElementById("sidemenu");
+
+function openmenu(){
+    sidemenu.style.right = "0";
+}
+function closemenu(){
+    sidemenu.style.right = "-200px"
+}
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwdoi89h6fW5FxejdqNhl5U-xWi_R3tMhXrr1uf9RxOXsYrXNlMsHxjF7ZPLLECOSGP/exec'
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById("msg")
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {
+        msg.innerHTML = "Message sent successfully"
+        setTimeout(function(){
+            msg.innerHTML = ""
+        },5000)
+        form.reset()
+      })
+      .catch(error => console.error('Error!', error.message))
+  })
